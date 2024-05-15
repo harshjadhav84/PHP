7d67d353d7576aa9e8efedce52f89c036a4aafd6
Harsh Jadhav <harshjadhav084@gmail.com>
2024-05-15 12:48:54 +0530

    Add files via upload

---
+++ String/string_function.php
@@ -0,0 +1,22 @@
+<?php
+
+$a = "HEllo world. It's a beautifual day,";
+$b = explode(" ", $a); // it will return string to array 
+echo "<pre>";
+var_dump($b);
+echo "</pre>";
+
+$str = "harsh jadhav";
+$array =  str_split($str);
+echo "<pre>";
+print_r($str);
+echo "</pre>";
+
+$p = "Harsh Jadhav";
+$q = strtolower($p);
+echo $q;
+
+?>
+
+//There are so many type of string function whic is can not possible to cover 
+in here so check next file there are all string function are available
